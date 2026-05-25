76e223ed610958faa6f3df6e7d4c7a2fe26b1e4e
kirushamatvieiev008 <mkiril087@gmail.com>
2026-05-25 21:17:52 +0300

    message

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+// function App() {
+
+//   return (
+//     <>
+      
+//     </>
+//   )
+// }
+
+// export default App
+
+
+export const App = () => {
+  return (
+    <>
+    <h1>hello world</h1>
+    </>
+  )
+}
+
+// export default App
